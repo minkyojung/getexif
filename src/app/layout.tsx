1668cd6721917e822052802b46041cb4bd9443f3
William Jung <williamjung0130@gmail.com>
2024-06-30 17:28:09 +0900

    add google Oauth login and supabase

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,6 +16,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <script src="https://accounts.google.com/gsi/client" async></script>
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   );
